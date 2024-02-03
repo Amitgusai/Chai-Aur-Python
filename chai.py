0ec301ef35feee17dd1f7e9d0e4ebf65949be06c
AmitGusai <amitgusai2003@gmail.com>
2024-02-03 14:01:41 +0530

    lecture_3 complete

--- chai.py
+++ chai.py
@@ -6,6 +6,12 @@ def tea(n):
 
 tea("WorkOut")
 
+tea_1 = "green tea" 
 
+tea_2 = "black tea" 
 
+tea_3 = "masala tea"
+
+# Variables also known as Attributes 
+# Modules are a folder in which codes from other person are stored together ( It has many methods(function))
 
