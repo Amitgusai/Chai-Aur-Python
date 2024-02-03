4f40c98a566c17058a1cc850d1cfc5480ca25946
AmitGusai <amitgusai2003@gmail.com>
2024-02-03 15:06:33 +0530

    Update lecture_4.py

--- lecture_4.py
+++ lecture_4.py
@@ -1,1 +1,28 @@
-# ..............................        Data Types
+# ..............................        Data Types Overview
+
+# Numbers : 1234, 1.43, 0b111, 3+4j, Decimal{}, Fraction{}
+
+# String : 'amit', "bat's", b'a/z01z', u'sp/xc4m' 
+
+# List :
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
