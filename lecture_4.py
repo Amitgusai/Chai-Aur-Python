cec86581f7a6f85ad8ad33b2411f28b9d0c91700
AmitGusai <amitgusai2003@gmail.com>
2024-02-06 09:18:01 +0530

    Update lecture_4.py

--- lecture_4.py
+++ lecture_4.py
@@ -1,12 +1,43 @@
 # ..............................        Data Types Overview
 
-# Numbers : 1234, 1.43, 0b111, 3+4j, Decimal{}, Fraction{}
 
-# String : 'amit', "bat's", b'a/z01z', u'sp/xc4m' 
+# Number : 1234, 3.1425, 3+4j, 0b111, Decimal(), Fraction(). 
+# String : 'span', "you're", b'a\x01x', u'sp/xc4m'(emoji's)
 
-# List :
+# list : [1, 4, 5, [4,5]], list(range(10)){method of list}
+# Tuple : (1, 4, 5,'span'), tuple('span'), namedtuple
+
+# In this Dictionary, value index does not start from 0, we can set it ourself 
+# Dictionary : {'food':'span', 'score':'90'}, dict(hours=10)
+
+# In set, it only contains unique values(not copy of the same value)
+# Set : set('abc'), {'a','b', 'c'}
+
+# File Machanism : open('eggs.txt'), open(r'C:\ham.bin', 'wb')
+
+# Boolean : True, False
+# None : None (no response)
+# Functions, modules, classes
+
+
+# To make exiting functionality more powerfull
+# Advance : Decorators, Generators, Iterators, MetaProgramming
 
 
 
+
+# ** : power of 
+
+# import math
+# import random (random value , choice(passing array between))
+
+# Internally, Strings is kind of treated as List But
+# String does not support value assignment unlike List : As it is immutable
+
+# dic = {'score':45, 'drink':'lemon', 'snacks':'peanuts'}
+# print(dic)
+# dic['score'] = 89
+# print(dic['score'])
+# print(dic)
 
 
