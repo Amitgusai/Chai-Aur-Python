0ecacbc02dbfd3f856082de7afea8776ad5ddaa7
AmitGusai <amitgusai2003@gmail.com>
2024-02-06 09:44:14 +0530

    Create lecture_5.py

---
+++ lecture_5.py
@@ -0,0 +1,21 @@
+# Any or all data ka type in python goes to memory not in the variable
+# which is why:
+# Python does not have any data type
+# But:
+# Memory me reference ka data type hota hai And memory me data ka type assign hota hai.
+
+# Example:                          _IMP_
+#     a = 5
+#     a ka data type kya hai : nothing
+#     5 ka data type : Number data type/object type
+
+# refcount: No particular method as such written on some documents
+
+# Python treats Strings and Numbers differently:
+#     Late garbage collection in case of String and Numbers for optimization as it can be used further in the program..
+
+
+
+
+
+
