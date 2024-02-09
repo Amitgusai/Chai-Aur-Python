0ba9bd90f8044bb4332dae6a62de606cd360bfd1
AmitGusai <amitgusai2003@gmail.com>
2024-02-09 19:28:47 +0530

    Update lecture-9.py

--- lecture-9.py
+++ lecture-9.py
@@ -37,7 +37,7 @@
 # >>> chai_type = {"chai": "black", "name": "rajwari", "cups": 2}
 # >>> chai_type
 # {'chai': 'black', 'name': 'rajwari', 'cups': 2}
-# >>> chai_type['name'] = 'rajasthani'
+# >>> chai_type['name'] = 'rajasthani'                                    Inserting another key:value pair
 # >>> chai_type
 # {'chai': 'black', 'name': 'rajasthani', 'cups': 2}
 
@@ -81,10 +81,92 @@
 
 
 
+# ....................................              Quesioning: 
+
+# >>> chai_type = {"masala": "spicy", "ginger": "mint", "green": "fresh"}
+
+# >>> print(chai_type)
+# {'masala': 'spicy', 'ginger': 'mint', 'green': 'fresh'}
+# >>> if "masala" in chai_type:
+# ...     print("It is spicy")
+# ...
+# It is spicy
 
 
 
 
+# ........................................              Methods: 
+
+# >>> print(len(chai_type))                                                     len() : no.of items(key:value pair)
+# 3
+
+# >>> chai_type.pop("masala")                                                   pop() : key must be mentioned as it is not in a sequential order.                      
+# 'spicy'
+
+# >>> print(chai_type)
+# {'ginger': 'mint', 'green': 'fresh'}
+
+
+# >>> chai_type = {'masala': 'spicy', 'ginger': 'mint', 'green': 'fresh'}
+
+# >>> chai_type.popitem()                                                       popitem() : item(key:value) removed from last value in dictionary
+# ('green', 'fresh')
+
+# >>> print(chai_type)
+# {'masala': 'spicy', 'ginger': 'mint'}
+
+# >>> chai_type_copy = chai_type                                                A new Reference copy is created
+# >>> chai_type_copy is chai_type                                               Reference are different
+# True
+
+
+
+
+# .......................................               Dictionaries   In   Dictionary
+
+
+# >>> star_bucks = {                                              
+# ... "coffee": {"masala": {"chai": "hot", "tea": "cold"}},                        Multiple dictionaries 
+# ... "latte": {"cappichino": "good", "year": 23}
+# ... }
+
+# >>> print(star_bucks)
+# {'coffee': {'masala': {'chai': 'hot', 'tea': 'cold'}}, 'latte': {'cappichino': 'good', 'year': 23}}
+
+
+# Accessing:
+
+
+# >>> star_bucks["coffee"]                                                         star_bucks(dictionary) accessing key(coffee) = result(dictionary)
+# {'masala': {'chai': 'hot', 'tea': 'cold'}}
+
+# >>> star_bucks["coffee"]["masala"]                                               star_bucks["coffee"](treating as whole variable) accessing key(masala)
+# {'chai': 'hot', 'tea': 'cold'}
+
+# >>> star_bucks["latte"]
+# {'cappichino': 'good', 'year': 23}
+
+# >>> star_bucks["latte"]["year"]                                                  Example
+# 23
+# >>> star_bucks["latte"]["cappichino"]
+# 'good'
+
+
+
+
+
+# ........................................                  Dictionary Comprehension (Calculation within Dictionary)
+
+
+# >>> squared = {x:x**2 for x in range(5)}                                         key must be mentioned (To make key:value pair)
+
+# >>> print(squared)
+# {0: 0, 1: 1, 2: 4, 3: 9, 4: 16}
+
+# >>> cube = {x : x++5 for x in range(10)}
+
+# >>> print(cube)
+# {0: 5, 1: 6, 2: 7, 3: 8, 4: 9, 5: 10, 6: 11, 7: 12, 8: 13, 9: 14}
 
 
 
