88dc07aac1084546842a487b3f400146b386aa48
AmitGusai <amitgusai2003@gmail.com>
2024-02-15 22:57:52 +0530

    Update lecture_14.py

--- lecture_14.py
+++ lecture_14.py
@@ -1,22 +1,80 @@
 # .................................................                 Functions
 
 
+# function(defination)    :   def 
+
+# def square(number):                                                  #parameters : placeholders
+#     ans = number ** 2
+#     return ans
+
+# ans = square(4)                                                             #arguments
+# print(ans)
+
+
+
+# ...........................................
+
+
+
+# def sum(num1, num2):
+#     add = num1 + num2
+#     return add
+
+# result = sum(4, 5)
+# print(result)
+
+
+
+# ........................................................
+
+
+
+# Polymorphism is handled by python internally
+
+# def multiply(para1, para2):
+#     multiply = para1 * para2
+#     return multiply
+
+# ans = multiply("and", 4)
+# ans1 = multiply(3, 5)
+# print(ans , ",", ans1)
 
 
 
 
+# ..........................................................
 
 
 
 
+# def radius(radius):
+#     area = 3.14 * radius ** 2
+#     circumferance = 2 * 3.14 * radius
+#     return area, circumferance                                        Can return two values, list etc.
+
+# a, b = radius(4)
+# print(a, ",", b)
+
+
+# How to make it to only two decimal places:                            HOMEWORK
 
 
 
+# ..........................................................
 
 
 
+# 5. Default Parameter Value:
+
+# def greet(name = "default value"):                                     This is a default value if no arguments is passed to the Function def
+#     return "Hello, " + name + "!"
+    
+# ans = greet()
+# print(ans)
 
 
+
+# ........................................................
 
 
 
