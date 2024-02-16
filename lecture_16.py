e65e61cad1d3ac164cb808d63bb462a50728ec55
AmitGusai <amitgusai2003@gmail.com>
2024-02-16 13:00:07 +0530

    Update lecture_16.py

--- lecture_16.py
+++ lecture_16.py
@@ -9,17 +9,18 @@
 # ................................................            1. Basic Class and Object 
 
 class Car:                                                                  # Convention : Capitalize the 'Name'
-    # Syntax:                                                               # To provide value as input
-    def __init__(self, brand, model):                                       # __init__() : it is a method by which --> we can pass Variables/Attributes (Also called 'Constructor' -> 1st method to be called)
-                                                                            # 'self' parameter -->  a reference to the current instance of the class, and is used to access variables that belongs to the class
+    # Syntax:                                                               # To provide input value or arguments from Object
+    def __init__(self, brand, model):                                       # The purpose of the init() method in a class is to initialize the attributes or variables of an object when it is created. [It is also known as the constructor method]
+                                                                            # By using the 'self' parameter, the method can access and assign values to the object's attributes, distinguishing them from the parameters passed to the method.
+                                                                            
         self.brand = brand                                                 
-        self.model = model                                                  # self.brand --> Differtiating between a class variable/attribute and parameters
+        self.model = model                                                  
     
     # pass        -->                                                       # pass(a Keyword) : a placeholder to store future code
     
-my_car = Car("Toyota", "Tata")                                              # Car() -> Syntax to create 'object' 
+my_car = Car("Toyota", "Tata")                                              # Car() -> Syntax to create 'object' and my_car is copy of Object : Car()
 
-print(my_car.brand)                                                         # to access variables/attributes of a class -> "."
+print(my_car.brand)                                                         # The code `print(my_car.brand)` is accessing the `brand` attribute of the `my_car` object using the dot notation '.' and printing its value.
 print(my_car.model)
 
 
