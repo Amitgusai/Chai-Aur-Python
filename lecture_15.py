62a96f10b859e4297f5564abeaa4c6f8540fad47
AmitGusai <amitgusai2003@gmail.com>
2024-02-16 10:54:26 +0530

    Update lecture_15.py

--- lecture_15.py
+++ lecture_15.py
@@ -14,33 +14,33 @@
 
 
 
-# username = "amit"
-# def test():
-#     username = "chai"                                                             # Global variable is applicable throught the python file
-#     print(username)                                                               # username = "chai" : dead after function is completed (only applicable within the declared function)
+username = "amit"
+def test():
+    username = "chai"                                                             # Global variable is applicable throught the python file
+    print(username)                                                               # username = "chai" : dead after function is completed (only applicable within the declared function)
 
-# print(username)
-# test()
+print(username)
+test()
 
 # //////////////////////////////////
 
-# x = 99
-# def func(y):
-#     z = x + y                                                                       # here x value is replaced by global variables
-#     return z
+x = 99
+def func(y):
+    z = x + y                                                                       # here x value is replaced by global variables
+    return z
 
-# ans = func(1)
-# print(ans)
+ans = func(1)
+print(ans)
 
 # ///////////////////////////////////
 
-# x = 99
-# def func3():
-#     global x                                                                          # Avoid using 'global' keyword
-#     x = 88                                                                            # Don't override the value, only access it (not a reliable code)
+x = 99
+def func3():
+    global x                                                                          # Avoid using 'global' keyword
+    x = 88                                                                            # Don't override the value, only access it (not a reliable code)
     
-# func3()
-# print(x)
+func3()
+print(x)
 
 
 
@@ -52,12 +52,12 @@
 def f1():
     x = 88 
     def f2():
-        print(x)                                                                        # Climbing concept : rooms -> house -> global    
-    return f2               # f2 : definition return ki hai
+        print(x)                                                                        # Climbing concept : rooms -> house -> global (variables)    
+    return f2                                                                           # f2 : definition return ki hai (reference)
 
 result = f1()
-# print(result)
-result()
+print(result)                                                                           # Reference of f2
+result()                                                                                # result() : calling f2 as a function
 
 
 
@@ -75,11 +75,5 @@ g = chaicoder(3)
 
 print(f(2))
 print(g(3))
-
-
-
-
-
-
 
 
