e960e1cc5364ffdeee81c741f8c0a142eb90019d
AmitGusai <amitgusai2003@gmail.com>
2024-02-17 16:00:34 +0530

    Update lecture_17.py

--- lecture_17.py
+++ lecture_17.py
@@ -7,11 +7,11 @@
 
 # Most Basic Decorator: 
     
-# def debug(func):
-#     def wrapper():
-#         return func()
+def debug(func):
+    def wrapper():
+        return func()
     
-#     return wrapper   
+    return wrapper   
 
 
 
@@ -21,28 +21,28 @@
 
 # Time a function takes to execute - using decorator
 
-# import time
+import time
 
-# #syntax: of making decorator
-# def timer(func):                                                                                                    # The `func` parameter in the `timer` function represents the original function(example_func) that is being timed.
+#syntax: of making decorator
+def timer(func):                                                                                                    # The `func` parameter in the `timer` function represents the original function(example_func) that is being timed.
     
-#     def wrapper(*args, **kwargs):                                                                                   # *args : arguments ,  **kwargs : keyword arguments
+    def wrapper(*args, **kwargs):                                                                                   # *args : arguments ,  **kwargs : keyword arguments
         
-#         start = time.time()                                                                                         # starting point time
-#         result = func(*args, **kwargs)                                                                              # calling the original function 'func' with argumets and storing in 'result' variable , allows the decorator to exucute the original funtion                                                                         
-#         end = time.time()                                                                                           # ending point time
+        start = time.time()                                                                                         # starting point time
+        result = func(*args, **kwargs)                                                                              # calling the original function 'func' with argumets and storing in 'result' variable , allows the decorator to exucute the original funtion                                                                         
+        end = time.time()                                                                                           # ending point time
         
-#         print(f"{func.__name__}ran in {end - start} time")                                                          # __name__ : name of the original function is accessed using 'name' attribute , Total time = end - start
-#         return result                                                                                               # To complete the execution of wrapper function, we returned something(result)
+        print(f"{func.__name__}ran in {end - start} time")                                                          # __name__ : name of the original function is accessed using 'name' attribute , Total time = end - start
+        return result                                                                                               # To complete the execution of wrapper function, we returned something(result)
     
-#     return wrapper                                                                                                  # The 'wrapper' function is being returned by the 'timer' decorator.
+    return wrapper                                                                                                  # The 'wrapper' function is being returned by the 'timer' decorator.
 
 
-# @timer                                                                                                              # now --> whenever we will call example_func function, it cannot be directly called : It will go through @timer decorator
-# def example_func(n):        
-#     time.sleep(n)
+@timer                                                                                                              # now --> whenever we will call example_func function, it cannot be directly called : It will go through @timer decorator
+def example_func(n):        
+    time.sleep(n)
     
-# example_func(2)                                                                                                     # In this case, the `example_func` function is passed as the `func` parameter when using the `@timer` decorator
+example_func(2)                                                                                                     # In this case, the `example_func` function is passed as the `func` parameter when using the `@timer` decorator
 
 
 
@@ -53,26 +53,26 @@
 # To print the function name and the values of its arguments 
 
 
-# def debug(func):
-#     def wrapper(*args, **kwargs):
-#         ans = func(*args, **kwargs)                                                                                     # To access name of the arguments :-
-#         args_value = ', '.join(str(args) for args in args)                                                              # join() : we get a iterable list --> (comprehension) applying looping --> str(arg) : value stored and converted to string
-#         kwargs_value = ', '.join(f"{key}={value} " for key, value in kwargs.items())
-#         print(f"calling {func.__name__} with args {args_value} and kwargs {kwargs_value}")
-#         return ans
+def debug(func):
+    def wrapper(*args, **kwargs):
+        ans = func(*args, **kwargs)                                                                                     # To access name of the arguments :-
+        args_value = ', '.join(str(args) for args in args)                                                              # join() : we get a iterable list --> (comprehension) applying looping --> str(arg) : value stored and converted to string
+        kwargs_value = ', '.join(f"{key}={value} " for key, value in kwargs.items())
+        print(f"calling {func.__name__} with args {args_value} and kwargs {kwargs_value}")
+        return ans
     
-#     return wrapper                                                                                                      # wrapper ki definition return hori to debug 
+    return wrapper                                                                                                      # wrapper ki definition return hori to debug 
 
-# @debug
-# def hello():
-#     print("hello")
+@debug
+def hello():
+    print("hello")
     
-# @debug
-# def greet(name, greeting = "Hello"):
-#     print(f"{greeting}, {name}")
+@debug
+def greet(name, greeting = "Hello"):
+    print(f"{greeting}, {name}")
   
-# hello()  
-# greet("chai", greeting="hanji")
+hello()  
+greet("chai", greeting="hanji")
 
 
 
@@ -80,28 +80,34 @@
 # ............................................................            3. Cache Return Value 
 
 
+#  It stores the results of function calls in a dictionary to avoid redundant computations when the same arguments are passed to the function again.
 
 import time
 
 def cache(func):
+    cache_value = {}                                                                                                    # should be written outside the wrapper funciton and storing the result
+                                                                                                                        # dictionary : In this accessing values are easy as compared to list(because of indexing in list)
     def wrapper(*args, **kwargs):
-        cache_value = {}        # dictionary : In this accessing values are easy as compared to list
-        print(cache_value)
+        
+        # print(cache_value)                                                                                              # Rough
+        
         if args in cache_value:
-            return cache_value[args]                                            # without executing the function again 
+            return cache_value[args]                                                                                    # without executing the function again 
         num = func(*args, **kwargs)
         cache_value[args] = num
         return num
+    
     return wrapper
 
 @cache
 def log_running_function(a, b):
-    time.sleep(4)
+    time.sleep(4)                                                                                                       # To better understand the decorator's working
     return a + b
 
-print(log_running_function(2,9))
-print(log_running_function(2,9))
-print(log_running_function(5,9))
+print(log_running_function(2, 3))       # same
+print(log_running_function(5, 9))
+print(log_running_function(2, 3))       # same
+
 
 
 
