f66c7378386eef7647df1111b1bf27e1ecad6ca6
AmitGusai <amitgusai2003@gmail.com>
2024-02-21 09:09:30 +0530

    Create random_api_2.py

---
+++ lecture_20/random_api_2.py
@@ -0,0 +1,34 @@
+import requests
+
+def fetch_random_jokes():
+    # url = "https://api.freeapi.app/api/v1/public/randomjokes/joke/random"
+    
+    response = requests.get(url="https://api.freeapi.app/api/v1/public/randomjokes/joke/random")
+    data = response.json()
+
+    if data["success"] and "data" in data:
+        user_data = data["data"]
+        jokes = user_data["content"]
+        return jokes
+    else:
+        raise Exception("Api failed to fetch")
+
+def main():
+    try:
+        content = fetch_random_jokes()
+        print("Joke (Smile): ", content)
+    except Exception:
+        print(str(Exception))
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
