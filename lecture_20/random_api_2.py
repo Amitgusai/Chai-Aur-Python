c8bf7586207cc3d9441422b26686a24632b47b72
AmitGusai <amitgusai2003@gmail.com>
2024-02-21 09:11:55 +0530

    Update random_api_2.py

--- lecture_20/random_api_2.py
+++ lecture_20/random_api_2.py
@@ -1,10 +1,13 @@
+# .......................................................         Practice -1         ......................................................
+
+
 import requests
 
 def fetch_random_jokes():
     # url = "https://api.freeapi.app/api/v1/public/randomjokes/joke/random"
     
     response = requests.get(url="https://api.freeapi.app/api/v1/public/randomjokes/joke/random")
-    data = response.json()
+    data = response.json()                                                                                  # Json typically convert the data in a dictionary (key:value pair)
 
     if data["success"] and "data" in data:
         user_data = data["data"]
