9150c8167581914ebaccc1e36092b8b15d5df29a
AmitGusai <amitgusai2003@gmail.com>
2024-02-22 13:38:01 +0530

    Virtual Environment Installation

---
+++ lecture_22_venv/notes.py
@@ -0,0 +1,21 @@
+
+# virtualenv : Virtual Environment 
+# General understanding : It is as almost a different computer within your system which contains python 
+# Does not affect or go to the main system's python
+
+
+# Then all the installed 'pip' list will send/store to 'requirement.txt' (standard)
+
+# venc : Name of the virtual environmet's name
+
+# For Professional / Production: with out virtual environment not possible
+
+# All the installation is done in the virtual environment 
+
+# How to install modules form requirement from requirement.txt
+
+
+# To activate:  .\.venv\Scripts\activate
+# To deactivate : deactivate 
+
+
