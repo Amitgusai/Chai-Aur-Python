6d0b3781eb3512d1c0aba6437a6f0c29a30193f3
AmitGusai <amitgusai2003@gmail.com>
2024-02-23 19:49:16 +0530

    Update Lecture_1.py

--- Lecture_1.py
+++ Lecture_1.py
@@ -7,15 +7,24 @@ from chai import tea
 tea("Lazy")
 
 # Usually the byte code(runs faster) is hidden from the developer BUT 
-# when we 'import' files or code from 'another file' then it is visible
+# when we 'import' files or code from 'another file' then the byte code is visible
+
+# Compiled to byte code 
 
 # cpython (default) : It uses and is a standard version of python (python = cpython)
 
 # 311 : Version name 
 
-# __pycache__ : To Organisize the files : .pyc {compiled (to byte cod)e : Technical term} for readibility
+# __pycache__ : To Organisize the files : .pyc {compiled python file} for readibility
 
+# .pyc : This compiled python is also known as 'Frozen Binaries'  
+# But:
 
+    """ 1. '.pyc' is a compiled python file that still requires the help of python interpreter to execute, meanwhile
+    2.'Frozen Binaries' is a standclone executable file which includes both python application and python interpreter."""
+    
+
+# Byte code runs faster: mostly check of syntax are done when converting to byte code 
 
 # .......................            Python Virtual Machine 
 
@@ -23,7 +32,7 @@ tea("Lazy")
 # also known as python interpreter 
 # run-time engine
 
-# Byte Code (instruction specific to Python Virtual Machine) is not a machine code (It does not directly instruct to Hardware) 
+""" Byte Code (instruction specific to Python Virtual Machine) is not a machine code (It does not directly instruct to Hardware)."""
 
 # Yes, your statement is correct. Bytecode, such as that used in the Java Virtual Machine (JVM) or Python Virtual Machine (PVM), is not the same as machine code. 
 # Bytecode is a form of instruction set designed for efficient execution by a software interpreter, in this case, the JVM or PVM. 
